be51913fa86754d07129507b131e57b7b2ef3067
Serah Njoroge <sarahnjoroge2015@gmail.com>
2023-09-14 13:40:00 +0300

    ....

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans">IBM Building, Seecond floor, Kimathi Street, Nairobi</p>
+        <p className="p__opensans">+254 700-000-254</p>
+        <p className="p__opensans">+254 700-000-255</p>
       </div>
 
       <div className="app__footer-links_logo">
@@ -32,14 +32,14 @@ const Footer = () => (
       <div className="app__footer-links_work">
         <h1 className="app__footer-headtext">Working Hours</h1>
         <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">08:00 am - 09:00 pm</p>
         <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
+        <p className="p__opensans">08:00 am - 04:00 pm</p>
       </div>
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">Copyright &copy; 2023 by Njoroge-s | All Rights Reserved.</p>
     </div>
 
   </div>
